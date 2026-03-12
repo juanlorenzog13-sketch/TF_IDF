f0dd985caa3cc567c29b3f9abc2ac8e7224478d8
juanlorenzog13-sketch <juanlorenzog13@gmail.com>
2026-03-12 13:53:26 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -17,7 +17,7 @@ La aplicación aplica normalización y *stemming* para que palabras como *playin
 # Ejemplo inicial en inglés
 text_input = st.text_area(
     "Escribe tus documentos (uno por línea, en inglés):",
-    "The dog barks loudly.\nThe cat meows at night.\nThe dog and the cat play together."
+    "Muchos años después, frente al pelotón de fusilamiento.\nel coronel Aureliano Buendía había de recordar aquella tarde remota.\nen que su padre lo llevó a conocer el hielo."
 )
 
 question = st.text_input("Escribe una pregunta (en inglés):", "Who is playing?")
